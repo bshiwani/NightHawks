12762be5054aaecd762c5d0abb320a415a45e70a
jtwilder <jeffwilder2@gmail.com>
2015-12-09 14:54:25 -0500

    Modified to ignore brightness thresholds and changed morphology a bit

--- colorBlobMasks.py
+++ colorBlobMasks.py
@@ -25,23 +25,29 @@ def showMe(name,image):
 
 # readThresholds reads the HSV thresholds saved in a text file
 def readThresholds(fileName):
-	minHSV = np.array([-1,-1,-1]) #Create arrays to store thresholds
-	maxHSV = np.array([-1,-1,-1])
-	try :
-	    f1 = open(fileName,"r")
-	    minHSV[0] = int(f1.readline()) #read min H
-	    minHSV[1] = int(f1.readline()) #read min S
-	    minHSV[2] = int(f1.readline()) #read min V
-	    maxHSV[0] = int(f1.readline())
-	    maxHSV[1] = int(f1.readline())
-	    maxHSV[2] = int(f1.readline())
-	    #print minHSV , maxHSV
-	    f1.close()
-	    return (minHSV,maxHSV)
-	except IOError:
-	    print 'Unable to open the threshold file\n'
-	    cv2.destroyAllWindows()
-	    sys.exit()
+    minHSV = np.array([0,0,0]) #Create arrays to store thresholds setting default threshold values
+    maxHSV = np.array([255,255,255])
+    ignoreV = True #Ignore brightness threshold
+    try :
+        f1 = open(fileName,"r")
+        minHSV[0] = int(f1.readline()) #read min H
+        minHSV[1] = int(f1.readline()) #read min S
+        minHSV[2] = int(f1.readline()) #read min V
+        maxHSV[0] = int(f1.readline())
+        maxHSV[1] = int(f1.readline())
+        maxHSV[2] = int(f1.readline())
+        
+        if(ignoreV): #Restore to default values if set to ignore brightness
+            minHSV[2] = 0
+            maxHSV[2] = 255
+            
+        #print(minHSV,maxHSV)
+        f1.close()
+        return (minHSV,maxHSV)
+    except IOError:
+        print 'Unable to open the threshold file\n'
+        cv2.destroyAllWindows()
+        sys.exit()
        
 #****************Main Code******************************************
 # Threshold filenames
@@ -50,25 +56,38 @@ yellowThreshFile = 'thresholds/yellowOut.txt'
 #blueThreshFile = 'blueHome.txt'
 #yellowThreshFile = 'yellowHome.txt'
 
+#Read thresholds from files:
+blueThresh = readThresholds(blueThreshFile)
+yellowThresh = readThresholds(yellowThreshFile)
+
+#Initialize video capture object
 cap = cv2.VideoCapture(0)
 
+#Start looping
 print('Press q to exit')
 while cv2.waitKey(1)& 0xFF != ord('q'): 
     ret,myImg = cap.read()   
     myImg = cv2.medianBlur(myImg,3)
     cv2.imshow('orig',myImg)
     
-    blues,maskList = hf.getHSVFilters(myImg,readThresholds(blueThreshFile),(11,11),2000000)
+    blues,maskList = hf.getHSVFilters(myImg,blueThresh,(5,5),2000000)
+    cv2.imshow("blues", blues)
     
-    if len(maskList) >0 : 
-        sorted(maskList,key = lambda m: m[1][0]['m00'] ,reverse=True)
+    
+    if len(maskList) >0 : #If there are blue objects found:
+        sorted(maskList,key = lambda m: m[1][0]['m00'] ,reverse=True) #Sort maskList according to size of blob
         M,center,vecta2,vectb1,vectb2,angle,Vect,L= maskList[0][1]	
+        #cv2.imshow('maskList 0', maskList[0][0])
         rotMat = cv2.getRotationMatrix2D( center, angle, 1.0 )    
         blues = cv2.warpAffine(blues, rotMat, (blues.shape[0],blues.shape[1]))
         myImg = cv2.warpAffine(myImg, rotMat, (blues.shape[0],blues.shape[1]))
+        
         cv2.imshow('filterB',blues)
-        yellows = hf.getHSVFilters(myImg,readThresholds(yellowThreshFile),(5,5),20000)
+        yellows = hf.getHSVFilters(myImg,yellowThresh,(5,5),20000)
         cv2.imshow('filterY',yellows[0])
+    
+    
+    
 
 cap.release()   
 cv2.destroyAllWindows()
